66ffec48cd9b644e89ee7c6553410053c0e49b17
Anna Remizova <55445666+apremizova@users.noreply.github.com>
2023-04-13 23:46:19 +0300

    solution ztest

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.weightstats import ztest
 
+chat_id = 370558433 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, pval = ztest(x,value = 300,alternative= 'smaller')
+    ans = True if pval < 0.04 else False
+    return ans # Ваш ответ, True или False
